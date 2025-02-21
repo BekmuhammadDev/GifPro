44741afd419a4c7f5f23debe7cf0fa93870a9634
Beekmuhammad <161308700+BekmuhammadDev@users.noreply.github.com>
2025-02-21 17:35:21 +0500

    ok

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ loader.computedStyleMap.display="block";
 document.querySelector(".wrapper").style.display = "none"
 
 let q = document.getElementById("search-box").value;
-let gifCount = 10;
+let gifCount = 30;
 let finalURL = `https://api.giphy.com/v1/gifs/search?api_key=${apikey}&q=${q}&limit=${gifCount}&offset=0&rating=g&lang=en`;
 document.querySelector(".wrapper").innerHTML = "";
 
